fb4b45904dcbe63fccb9ffb458d3894ddcc6c392
Matheus Rodrigues de Lima <mhs.djc@gmail.com>
2025-06-15 17:24:02 -0300

    add pdf

--- emailService.js
+++ emailService.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const nodemailer = require('nodemailer');
 
 // Configuração do transporte de e-mail (aqui usamos Gmail como exemplo)
@@ -11,11 +13,18 @@ const transporter = nodemailer.createTransport({
 
 // Função para enviar e-mail
 const sendEmail = (to, subject, text) => {
+  const pdfPath = path.join(__dirname, 'arquivos', 'seuarquivo.pdf');
   const mailOptions = {
     from: 'mhs.djc@gmail.com',
     to,
     subject,
-    text
+    text,
+    attachments: [
+                {
+                    filename: 'documento.pdf',
+                    path: pdfPath
+                }
+            ]
   };
 
   return transporter.sendMail(mailOptions);
